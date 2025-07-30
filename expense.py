cabdc10b02a297b9e2311f7ff6b06fa0875c17b7
Yedla Laxmi Charishma <142088544+Laxmicharishma2005@users.noreply.github.com>
2025-07-30 23:46:28 +0530

    Delete expense.py

--- expense.py
+++
@@ -1,9 +0,0 @@
-class Expense:
-    def __init__(self, name, category, amount, day):
-        self.name = name
-        self.category = category
-        self.amount = amount
-        self.day = day
-
-    def __repr__(self):
-        return f"<Expense: {self.name}, {self.category}, ₹{self.amount:.2f}, Day {self.day}>"
